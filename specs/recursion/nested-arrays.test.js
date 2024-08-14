447ab9284b378cc8c4ec78d52f0364aa5171acb3
Keano Robinson <keano.robinson@hotmail.com>
2024-08-14 12:32:48 +0100

    Implement solutions to recursive exercises.

--- specs/recursion/nested-arrays.test.js
+++ specs/recursion/nested-arrays.test.js
@@ -10,7 +10,9 @@
  */
 
 function nestedAdd(array) {
-  // write code here
+  if (array.length === 0) return 0;
+  if (Array.isArray(array[0])) return nestedAdd(array[0]) + nestedAdd(array.slice(1));
+  return array[0] + nestedAdd(array.slice(1));
 }
 
 test.skip("nested arrays addition", () => {
