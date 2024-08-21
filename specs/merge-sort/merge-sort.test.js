f431f3d016ac311796136586ccc9e628f0453c2e
Keano Robinson <keano.robinson@hotmail.com>
2024-08-21 11:57:19 +0100

    implement merge sort

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -6,8 +6,28 @@
   You'll need to write more than just one function
 */
 
+// No difference between the best, average and worst case: always O(n*log(n))
+// Most JS engines use merge sort under the hood
+// If you don't know what's coming in, merge sort is pretty performant 
+// Skipping comparisons since -- you know that everything past the first element in the sorted array is larger, so you gain that extra info
+// Spacial complexity is O(n), since it breaks down the original array into its separate elements -- this is relatively high
 const mergeSort = (nums) => {
-  // code goes here
+  if (!Array.isArray(nums)) return nums;
+
+  const merge = (sortedA, sortedB) => {
+    const mergeArr = [];
+    while (sortedA.length && sortedB.length) {
+      if (sortedA[0] < sortedB[0]) mergeArr.push(sortedA.shift());
+      else mergeArr.push(sortedB.shift());
+    }
+    return [...mergeArr, ...sortedA, ...sortedB];
+  }
+
+  if (nums.length < 2) return nums;
+  const splitIdx = Math.ceil(nums.length / 2);
+  const A = nums.slice(0, splitIdx);
+  const B = nums.slice(splitIdx, nums.length);
+  return merge(mergeSort(A), mergeSort(B))
 };
 
 // unit tests
