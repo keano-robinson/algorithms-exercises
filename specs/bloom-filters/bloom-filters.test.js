6780ebc245411fa4de385a3b8e5e7b708ddaeb09
Keano Robinson <keano.robinson@hotmail.com>
2024-10-13 13:18:15 +0100

    Bloom filter implementation

--- specs/bloom-filters/bloom-filters.test.js
+++ specs/bloom-filters/bloom-filters.test.js
@@ -3,60 +3,66 @@
 // instances of that as your hashing functions
 const XXH = require("xxhashjs");
 const h1 = (string) =>
-  Math.abs(XXH.h32(0xabcd).update(string).digest().toNumber() % 100);
+	Math.abs(XXH.h32(0xabcd).update(string).digest().toNumber() % 100);
 const h2 = (string) =>
-  Math.abs(XXH.h32(0x1234).update(string).digest().toNumber() % 100);
+	Math.abs(XXH.h32(0x1234).update(string).digest().toNumber() % 100);
 const h3 = (string) =>
-  Math.abs(XXH.h32(0x6789).update(string).digest().toNumber() % 100);
+	Math.abs(XXH.h32(0x6789).update(string).digest().toNumber() % 100);
 
 // fill out these two methods
 // `add` adds a string to the bloom filter and returns void (nothing, undefined)
 // `contains` takes a string and tells you if a string is maybe in the bloom filter
 class BloomFilter {
-  // you'll probably need some instance variables
-  add(string) {
-    // code here
-  }
-  contains(string) {
-    // code here
-  }
+	#array = new Array(100).fill(0);
+	add(string) {
+		[h1, h2, h3].forEach(
+			(hashFunction) => (this.#array[hashFunction(string)] = 1)
+		);
+	}
+	contains(string) {
+		return [h1, h2, h3].reduce(
+			(mayHaveSeen, hashFunction) =>
+				mayHaveSeen && !!this.#array[hashFunction(string)],
+			true
+		);
+	}
 }
 
 // unit tests
 // do not modify the below code
 describe.skip("BloomFilter", function () {
-  let bf;
-  beforeEach(() => {
-    bf = new BloomFilter();
-  });
-  test.skip("returns false when empty", () => {
-    expect(bf.contains("Brian")).toBe(false);
-    expect(bf.contains("Sarah")).toBe(false);
-    expect(bf.contains("Simona")).toBe(false);
-  });
-  test.skip("handles one item", () => {
-    expect(bf.contains("Brian")).toBe(false);
-    bf.add("Brian");
-    expect(bf.contains("Brian")).toBe(true);
-    expect(bf.contains("Sarah")).toBe(false);
-    expect(bf.contains("Simona")).toBe(false);
-  });
-  test.skip("handles many items", () => {
-    const names = [
-      "Brian",
-      "Simona",
-      "Sarah",
-      "Asim",
-      "John",
-      "Sean",
-      "Jessie",
-      "Paige",
-      "Ashley"
-    ];
-    names.forEach((item) => bf.add(item));
-    names.forEach((item) => expect(bf.contains(item)).toBe(true));
-    ["Sam", "Chris", "Taylor", "Florence"].forEach((item) =>
-      expect(bf.contains(item)).toBe(false)
-    );
-  });
+	let bf;
+	beforeEach(() => {
+		bf = new BloomFilter();
+	});
+	test("returns false when empty", () => {
+		expect(bf.contains("Brian")).toBe(false);
+		expect(bf.contains("Sarah")).toBe(false);
+		expect(bf.contains("Simona")).toBe(false);
+	});
+	test("handles one item", () => {
+		expect(bf.contains("Brian")).toBe(false);
+		bf.add("Brian");
+		expect(bf.contains("Brian")).toBe(true);
+		expect(bf.contains("Sarah")).toBe(false);
+		expect(bf.contains("Simona")).toBe(false);
+	});
+	test("handles many items", () => {
+		const names = [
+			"Brian",
+			"Simona",
+			"Sarah",
+			"Asim",
+			"John",
+			"Sean",
+			"Jessie",
+			"Paige",
+			"Ashley",
+		];
+		names.forEach((item) => bf.add(item));
+		names.forEach((item) => expect(bf.contains(item)).toBe(true));
+		["Sam", "Chris", "Taylor", "Florence"].forEach((item) =>
+			expect(bf.contains(item)).toBe(false)
+		);
+	});
 });
